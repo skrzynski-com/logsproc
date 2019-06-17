c9d33d3123063e982845d0d85b06827f0443e757
Wojciech Skrzyński <reg2017@skrzynski.com>
2019-06-17 17:04:47 +0200

    Refactoring

--- src/main/java/com/skrzynski/logsproc/LogsService.java
+++ src/main/java/com/skrzynski/logsproc/LogsService.java
@@ -12,8 +12,6 @@ import java.nio.file.Path;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 import static java.nio.file.Files.lines;
 import static java.util.concurrent.CompletableFuture.completedFuture;
@@ -26,7 +24,7 @@ public class LogsService {
     private final ObjectMapper objectMapper;
     private final Semaphore throttle;
     private final AtomicInteger counter = new AtomicInteger();
-    private final static Logger LOGGER = LoggerFactory.getLogger(LogsService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogsService.class);
 
     public LogsService(LogsMapper logsMapper, ObjectMapper objectMapper, @Value("${throttleLimit}") int throttleLimit) {
         this.logsMapper = logsMapper;
@@ -35,67 +33,66 @@ public class LogsService {
     }
 
     public void loadData(Path path) {
-        LOGGER.debug("loadData() started with path: " + path);
+        LOGGER.debug("loadData() started with path: {}", path);
         try {
             counter.set(0); // this is command line program, that is why there is only one counter
             final CompletableFuture<Void> completionDetector = completedFuture(null);
             lines(path) // NOSONAR, stream is closed by reduction
+                    .peek(this::throttleStream)
                     .reduce(completionDetector, (f, line) -> processLine(line, f), (f1, f2) -> f2)
                     .get(); // wait for the end of processing
-            LOGGER.info("loadData() has loaded: " + counter.get() + "lines");
+            LOGGER.info("loadData() has loaded: {} lines", counter.get());
         } catch (Exception e) {
-            LOGGER.error("loadData() encountered problem: " + e.getMessage(), e);
+            LOGGER.error("loadData() encountered problem: {}", e.getMessage());
+            throw new LogsServiceException(e);
+        }
+    }
+
+
+    private void throttleStream(String line) { // NOSONAR siganture required by peek function
+        try {
+            throttle.acquire();
+        } catch (InterruptedException e) { // NOSONAR, this is not service but command line application, so there will no interruption
+            LOGGER.error("throttleStream() encountered problem: {}", e.getMessage());
             throw new LogsServiceException(e);
         }
     }
 
     private CompletableFuture<Void> processLine(String line, CompletableFuture<Void> completionDetector) {
-        LOGGER.debug("processLine() started with line: " + line);
-        CompletableFuture<Void> result = completionDetector;
-        try {
-            throttle.acquire();
-            CompletableFuture<Void> future = supplyAsync(() -> deserializeLogEntry(line))
-                    .thenApplyAsync(persistLine())
-                    .thenAcceptAsync(releaseThrottle());
-
-            result = result.thenCompose(aVoid -> future);
-        } catch (InterruptedException e) { // NOSONAR, this is not service but command line application, so there will no interruption
-            LOGGER.error("processLine() encountered problem: " + e.getMessage(), e);
-            throw new LogsServiceException(e);
-        }
-
-        return result;
+        LOGGER.debug("processLine() started with line: {}", line);
+        return completionDetector.thenCompose(aVoid -> processLine(line));
     }
 
-    private Function<LogEntry, LogEntry> persistLine() {
-        return logEntry -> {
-            try {
-                LOGGER.debug("processLine() will insert: " + logEntry);
-                logsMapper.insert(logEntry);
-            } catch (DuplicateKeyException e) {
-                // first event has been already inserted
-                LOGGER.debug("processLine() will update duration and alert flag: " + logEntry);
-                logsMapper.updateDuration(logEntry);
-                logsMapper.updatAlert(logEntry);
-            }
-            return logEntry;
-        };
-    }
-
-    private Consumer<LogEntry> releaseThrottle() {
-        return le -> {
-            throttle.release();
-            counter.incrementAndGet();
-        };
+    private CompletableFuture<Void> processLine(String line) {
+        return supplyAsync(() -> deserializeLogEntry(line))
+                .thenAcceptAsync(this::persistLogEntry)
+                .thenRunAsync(this::releaseThrottle);
     }
 
     private LogEntry deserializeLogEntry(String line) {
         try {
             return objectMapper.readValue(line, LogEntry.class);
         } catch (IOException e) {
-            LOGGER.error("deserializeLogEntry() encountered problem: " + e.getMessage() + " with line: " + line, e);
+            LOGGER.error("deserializeLogEntry() encountered problem: {} with line: {}", e.getMessage(), line);
             throw new LogsServiceException(e);
         }
+    }
+
+    private void persistLogEntry(LogEntry logEntry) {
+        try {
+            LOGGER.debug("persistLogEntry() will insert: {}", logEntry);
+            logsMapper.insert(logEntry);
+        } catch (DuplicateKeyException e) {
+            // first event has been already inserted
+            LOGGER.debug("persistLogEntry() will update duration and alert flag: {}", logEntry);
+            logsMapper.updateDuration(logEntry);
+            logsMapper.updatAlert(logEntry);
+        }
+    }
+
+    private void releaseThrottle() {
+        throttle.release();
+        counter.incrementAndGet();
     }
 
     public static class LogsServiceException extends RuntimeException {
